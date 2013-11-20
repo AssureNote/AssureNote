73c0d19205fb6efb264d04f002e140ceb17e80a2
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-11-20 12:40:58 +0900

    Updated LibAssureNoteParser

---
+++ AssureNoteParser/src/TypeScript/LibAssureNoteParser.ts
@@ -0,0 +1,116 @@
+class Iterator<T> {//FIX ME!!
+}
+
+interface Array {
+        get(index: number): any;
+        set(index: number, value: any): void;
+        add(obj: any): void;
+        add(index: number, obj : any): void;
+        size(): number;
+        clear(): void;
+        remove(index: number): any;
+        iterator(): Iterator<Array>;
+}
+
+Object.defineProperty(Array.prototype, "size", {
+        enumerable : false,
+        value : function() {
+                return this.length;
+        }
+});
+
+Object.defineProperty(Array.prototype, "add", {
+        enumerable : false,
+        value : function(arg1) {
+                if(arguments.length == 1) {
+                        this.push(arg1);
+                } else {
+                        var arg2 = arguments[1];
+                        this.splice(arg1, 0, arg2);
+                }
+        }
+});
+
+Object.defineProperty(Array.prototype, "get", {
+        enumerable : false,
+        value : function(i) {
+                if(i >= this.length){
+                        throw new RangeError("invalid array index");
+                }
+                return this[i];
+        }
+});
+
+Object.defineProperty(Array.prototype, "set", {
+        enumerable : false,
+        value : function(i, v) {
+                this[i] = v;
+        }
+});
+
+Object.defineProperty(Array.prototype, "remove", {
+        enumerable : false,
+        value : function(i) {
+                if(i >= this.length){
+                        throw new RangeError("invalid array index");
+                }
+                var v = this[i];
+                this.splice(i, 1);
+                return v;
+        }
+});
+
+Object.defineProperty(Array.prototype, "clear", {
+        enumerable : false,
+        value : function() {
+                this.length = 0;
+        }
+});
+
+interface Object {
+        equals(other: any): boolean;
+        InstanceOf(klass: any): boolean;
+}
+
+Object.defineProperty(Object.prototype, "equals", {
+        enumerable : false,
+        value : function(other) {
+                return (this === other);
+        }
+});
+
+Object.defineProperty(Object.prototype, "InstanceOf", {
+        enumerable : false,
+        value : function(klass) {
+                return (<any>this).constructor == klass;
+        }
+});
+
+interface String {
+        startsWith(key: string): boolean;
+        endsWith(key: string): boolean;
+        lastIndexOf(ch: number) : number;
+        indexOf(ch: number) : number;
+        substring(BeginIdx : number, EndIdx : number) : string;
+}
+
+Object.defineProperty(String.prototype, "startsWith", {
+        enumerable : false,
+        value : function(key) {
+                return this.indexOf(key, 0) == 0;
+        }
+});
+
+Object.defineProperty(String.prototype, "endsWith", {
+        enumerable : false,
+        value : function(key) {
+                return this.lastIndexOf(key, 0) == 0;
+        }
+});
+
+Object.defineProperty(String.prototype, "equals", {
+        enumerable : false,
+        value : function(other) {
+                return (this == other);
+        }
+});
