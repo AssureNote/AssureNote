3a113f78bbdf8ddc3dab51a6fb0f73a89bbd96e4
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-12-14 15:35:08 +0900

    added socket.io.js

--- AssureNote/plugin/MessageChat/MessageChat.js
+++ AssureNote/plugin/MessageChat/MessageChat.js
@@ -1,6 +1,3 @@
-///<reference path="../../src/AssureNoteParser.ts" />
-///<reference path="../../src/Plugin.ts" />
-///<reference path="../../src/Editor.ts" />
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -21,5 +18,21 @@ var AssureNote;
         return MessageChatPlugin;
     })(AssureNote.Plugin);
     AssureNote.MessageChatPlugin = MessageChatPlugin;
+
+    var ConnectServerPlugin = (function (_super) {
+        __extends(ConnectServerPlugin, _super);
+        function ConnectServerPlugin(AssureNoteApp) {
+            _super.call(this);
+            this.AssureNoteApp = AssureNoteApp;
+        }
+        ConnectServerPlugin.prototype.ExecCommand = function (AssureNoteApp, Args) {
+            if (!io || !io.connect)
+                return;
+
+            var socket = io.connect('http://localhost:443');
+        };
+        return ConnectServerPlugin;
+    })(AssureNote.Plugin);
+    AssureNote.ConnectServerPlugin = ConnectServerPlugin;
 })(AssureNote || (AssureNote = {}));
 //# sourceMappingURL=MessageChat.js.map
--- AssureNote/plugin/MessageChat/MessageChat.ts
+++ AssureNote/plugin/MessageChat/MessageChat.ts
@@ -2,6 +2,8 @@
 ///<reference path="../../src/Plugin.ts" />
 ///<reference path="../../src/Editor.ts" />
 
+declare var io: any;
+
 module AssureNote {
     export class MessageChatPlugin extends Plugin {
         public EditorUtil: EditorUtil;
@@ -13,4 +15,16 @@ module AssureNote {
 			$.notify(Args.join(" "), "info");
 		}
 	}
+
+    export class ConnectServerPlugin extends Plugin {
+        constructor(public AssureNoteApp: AssureNoteApp) {
+            super();
+        }
+        ExecCommand(AssureNoteApp: AssureNoteApp, Args: string[]): void {
+            /* Check the existence of socked.io.js */
+            if (!io || !io.connect) return;
+
+            var socket = io.connect('http://localhost:443');
+        }
+    }
 }
