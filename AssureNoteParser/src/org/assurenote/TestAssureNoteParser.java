2a57a495bd387d11daf89fe7a4db15099f697e29
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-12-25 15:40:31 +0900

    added junit 4.11

---
+++ AssureNoteParser/src/org/assurenote/TestAssureNoteParser.java
@@ -0,0 +1,21 @@
+package org.assurenote;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class TestAssureNoteParser {
+
+	@Test
+	public void test1() {
+		assertNotNull("Not yet implemented");
+		
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals(1, 1+0);
+	}
+
+}
