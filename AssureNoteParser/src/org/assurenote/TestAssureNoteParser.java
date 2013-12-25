1055d7b9b1469d959c935622a653e6285a702e13
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-12-25 16:44:21 +0900

    added tests

--- AssureNoteParser/src/org/assurenote/TestAssureNoteParser.java
+++ AssureNoteParser/src/org/assurenote/TestAssureNoteParser.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.io.File;
+import java.io.FilenameFilter;
+
 import org.junit.Test;
 
 public class TestAssureNoteParser {
@@ -11,7 +14,6 @@ public class TestAssureNoteParser {
 	@Test
 	public void AlwaysPassed() {
 		assertNotNull("Non-Null String");
-		
 	}
 	
 	@Test
@@ -23,6 +25,7 @@ public class TestAssureNoteParser {
 	@Test
 	public void ParseGoal() {
 		String input = "*G";
+		
 		GSNRecord MasterRecord = new GSNRecord();
 		MasterRecord.Parse(input);
 		
@@ -38,6 +41,7 @@ public class TestAssureNoteParser {
 	@Test
 	public void ParseGoalWithContext() {
 		String input = "*G\n*C";
+		
 		GSNRecord MasterRecord = new GSNRecord();
 		MasterRecord.Parse(input);
 		
@@ -54,8 +58,71 @@ public class TestAssureNoteParser {
 		assertEquals(SubNode.NodeType, GSNType.Context);
 		assertNull(SubNode.SubNodeList);
 	}
-
+	
 	@Test
 	public void ParseNestedGoal() {
+		String input = "*G\n*S\n**G";
+		
+		GSNRecord MasterRecord = new GSNRecord();
+		MasterRecord.Parse(input);
+		
+		GSNDoc LatestDoc = MasterRecord.GetLatestDoc();
+		GSNNode TopGoal = LatestDoc.TopGoal;
+		assertNotNull(LatestDoc);
+		
+		assertNotNull(TopGoal);
+		assertEquals(TopGoal.NodeType, GSNType.Goal);
+		assertNotNull(TopGoal.SubNodeList);
+		assertEquals(TopGoal.SubNodeList.size(), 1);
+		
+		GSNNode SubNode = TopGoal.SubNodeList.get(0);
+		assertEquals(SubNode.NodeType, GSNType.Strategy);
+		assertNotNull(SubNode.SubNodeList);
+		assertEquals(SubNode.SubNodeList.size(), 1);
+		
+		SubNode = SubNode.SubNodeList.get(0);
+		assertEquals(SubNode.NodeType, GSNType.Goal);
+		assertNull(SubNode.SubNodeList);
+	}
+
+	@Test
+	public void ParseMetaData() {
+		String input = "Author:: test\nRole:: -\nDate:: 2000-01-01T00:00:00+0900\n*G\n*S\n**G";
+		
+		GSNRecord MasterRecord = new GSNRecord();
+		MasterRecord.Parse(input);
+		
+		GSNDoc LatestDoc = MasterRecord.GetLatestDoc();
+		GSNNode TopGoal = LatestDoc.TopGoal;
+		GSNHistory History = LatestDoc.DocHistory;
+		
+		assertNotNull(TopGoal);
+		
+		assertNotNull(History);
+		assertEquals(History.Author, "test");
+		assertEquals(History.Role, "-");
+		assertEquals(History.Date, "2000-01-01T00:00:00+0900");
+	}
+
+	class WGSNFilter implements FilenameFilter {
+		public boolean accept(File file, String name) {
+			return name.endsWith(".wgsn");
+		}
+	}
+	
+	@Test
+	public void ParseAllSamples() {
+		String path = "./sample/";
+		File dir = new File(path);
+		File[] files = dir.listFiles(new WGSNFilter());
+		for (File file : files) {
+			if (!file.canRead()) continue;
+			
+			GSNRecord MasterRecord = new GSNRecord();
+			MasterRecord.Parse(Lib.ReadFile(path + file.getName()));
+			GSNNode TopGoal = MasterRecord.GetLatestDoc().TopGoal;
+			
+			assertNotNull(TopGoal);
+		}
 	}
 }
