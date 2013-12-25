11a721daac4f8cbcf298f9ce6964ada8cd306445
Shunsuke Shida <shunsuke.sida@gmail.com>
2013-12-25 15:56:33 +0900

    Added tests

--- AssureNoteParser/src/org/assurenote/TestAssureNoteParser.java
+++ AssureNoteParser/src/org/assurenote/TestAssureNoteParser.java
@@ -2,20 +2,60 @@ package org.assurenote;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
 public class TestAssureNoteParser {
 
 	@Test
-	public void test1() {
-		assertNotNull("Not yet implemented");
+	public void AlwaysPassed() {
+		assertNotNull("Non-Null String");
 		
 	}
 	
 	@Test
-	public void test2() {
-		assertEquals(1, 1+0);
+	public void Instantiation() {
+		GSNRecord MasterRecord = new GSNRecord();
+		assertNotNull(MasterRecord);
+	}
+	
+	@Test
+	public void ParseGoal() {
+		String input = "*G";
+		GSNRecord MasterRecord = new GSNRecord();
+		MasterRecord.Parse(input);
+		
+		GSNDoc LatestDoc = MasterRecord.GetLatestDoc();
+		GSNNode TopGoal = LatestDoc.TopGoal;
+		assertNotNull(LatestDoc);
+		
+		assertNotNull(TopGoal);
+		assertEquals(TopGoal.NodeType, GSNType.Goal);
+		assertNull(TopGoal.SubNodeList);
+	}
+	
+	@Test
+	public void ParseGoalWithContext() {
+		String input = "*G\n*C";
+		GSNRecord MasterRecord = new GSNRecord();
+		MasterRecord.Parse(input);
+		
+		GSNDoc LatestDoc = MasterRecord.GetLatestDoc();
+		GSNNode TopGoal = LatestDoc.TopGoal;
+		assertNotNull(LatestDoc);
+		
+		assertNotNull(TopGoal);
+		assertEquals(TopGoal.NodeType, GSNType.Goal);
+		assertNotNull(TopGoal.SubNodeList);
+		assertEquals(TopGoal.SubNodeList.size(), 1);
+		
+		GSNNode SubNode = TopGoal.SubNodeList.get(0);
+		assertEquals(SubNode.NodeType, GSNType.Context);
+		assertNull(SubNode.SubNodeList);
 	}
 
+	@Test
+	public void ParseNestedGoal() {
+	}
 }
