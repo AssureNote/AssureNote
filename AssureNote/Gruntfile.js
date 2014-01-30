0306c811d29f0be267067f58851cf1762359c136
uchida <uchida.atsushi.8@gmail.com>
2014-01-30 18:24:54 +0900

    add Gruntfile.js in client side. ref #232
    add sourcemap. ref #232

---
+++ AssureNote/Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+    	yuidoc: {
+            compile: {
+                name: 'AssureNote',
+                description: 'AssureNote Server auto-generated document',
+                version: '0.0.1',
+                url: 'https://github.com/AssureNote/AssureNote',
+                options: {
+                    name: 'AssureNote',
+                    description: 'AssureNote auto-generated document',
+                    version: '0.0.1',
+                    url: 'https://github.com/AssureNote/AssureNote',
+                    options: {
+                        linkNatives: 'true',        
+                        attributesEmit: 'true',
+                        selleck: 'true',
+                        paths: './src',
+                        outdir: './doc'
+                    }
+                }
+    		}
+    	},
+
+        jade: {
+            compile: {
+                files: {
+                    'index.html': './index.jade'
+                }
+            }
+
+        },
+
+        //TODO use grunt-typescript (@compile_list cannot use in grunt-typescript)
+        exec: {
+            typescript: {
+                cmd: 'tsc @compile_list --module "commonjs" --sourcemap'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-yuidoc');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('default', ['jade:compile', 'exec:typescript']);
+};
